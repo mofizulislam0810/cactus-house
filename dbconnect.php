dd3aa5da783a0e3d892cfc7e6a8ba2d2020f7c9d
rifat7030 <mofizul.islam0810@gmail.com>
2022-01-18 11:17:36 +0600

    second

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
- $connection = mysqli_connect("sql304.epizy.com","epiz_30566716","SV5h4qC6SKekOl","epiz_30566716_cactus") or
+ $connection = mysqli_connect("localhost","root","0810","cactus") or
  die("Error".mysqli_error($connection));
  ?>
